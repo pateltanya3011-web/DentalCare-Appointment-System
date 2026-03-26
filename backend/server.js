e24c4bd4dc2454d8242c3065d5b658064d4b93ee
Tanya Patel <pateltanya3011@gmail.com>
2026-03-26 16:03:14 +0530

    link fixed

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,19 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: "*"
-}));
+app.use(
+  cors({
+    origin: [
+      'https://pathlab-frontend-nine.vercel.appdental-care-appointment-system-c1jg-njbbfi2i9.vercel.app',
+      'http://localhost:5173',
+      'dental-care-appointment-system-c1jg.vercel.app'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['set-cookie'],
+  })
+)
 
 //test
 
